b6f1d27025b0261cf58f2ca0c86c7b57682ba442
Jamie McCann <jamie.mccann@umu.se>
2025-03-31 14:58:43 +0200

    Copying over api endpoints from pg-heatmaps

--- src/plantgenie_api/main.py
+++ src/plantgenie_api/main.py
@@ -4,19 +4,38 @@ import uuid
 from pathlib import Path
 from typing import Annotated, Literal
 
+import duckdb
+
+from celery.result import AsyncResult
 from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, FileResponse
 from fastapi.exceptions import HTTPException
-from celery.result import AsyncResult
+
+from plantgenie_api.models import (
+    AnnotationsRequest,
+    AnnotationsResponse,
+    AvailableGenome,
+    AvailableGenomesResponse,
+    AvailableSpecies,
+    AvailableSpeciesResponse,
+    ExpressionRequest,
+    ExpressionResponse,
+    GeneAnnotation,
+    GeneInfo,
+    SampleInfo,
+)
+
 from plantgenie_api.tasks import (
     fake_blast_task,
     real_blast_task,
-)  # Import the Celery task
+)
 
 DATA_PATH = (
     Path(os.environ.get("DATA_PATH")) or Path(__file__).parent.parent / "example_data"
 )
+
+DATABASE_PATH = DATA_PATH / "upsc-plantgenie.db"
 
 BLAST_DB_MAPPER = {
     "Picea abies": {
@@ -206,3 +225,185 @@ def retrieve_blast_result_as_html(job_id: str):
         status_code=404,
         detail=f"html result for requested job id - {job_id} - was not found",
     )
+
+
+
+@app.get("/api/available-species")
+async def get_available_species() -> AvailableSpeciesResponse:
+    with duckdb.connect(DATABASE_PATH, read_only=True) as connection:
+        query_relation = connection.sql("select * from species;")
+
+        return AvailableSpeciesResponse(
+            species=[
+                AvailableSpecies(
+                    **{
+                        k: v
+                        for k, v in zip(AvailableSpecies.model_fields.keys(), result)
+                    }
+                )
+                for result in query_relation.fetchall()
+            ]
+        )
+
+
+@app.get("/api/available-genomes")
+async def get_available_genomes() -> AvailableGenomesResponse:
+    with duckdb.connect(DATABASE_PATH, read_only=True) as connection:
+        query_relation = connection.sql("select * from genomes join species on (genomes.species_id = species.id);").project(
+            "id", "species_id", "species_name", "version", "publication_date", "doi"
+        )
+        print(query_relation)
+
+        return AvailableGenomesResponse(
+            genomes=[
+                AvailableGenome(
+                    **{
+                        k: v
+                        for k, v in zip(AvailableGenome.model_fields.keys(), result)
+                    }
+                )
+                for result in query_relation.fetchall()
+            ]
+        )
+
+
+@app.post("/api/annotations")
+async def get_annotations_duckdb(request: AnnotationsRequest) -> AnnotationsResponse:
+    if len(request.gene_ids) == 0:
+        return AnnotationsResponse(results=[])
+
+    gene_ids = GeneInfo.split_gene_ids_from_request(request.gene_ids)
+
+    # Map gene_ids to their original order for sorting results later
+    gene_id_order = {
+        (gene.chromosome_id, gene.gene_id): i for i, gene in enumerate(gene_ids)
+    }
+
+    # build query template with (?, ?) for each gene_id
+    query = (
+        "SELECT * FROM annotations WHERE (chromosome_id, gene_id) IN ("
+        + ", ".join(["(?, ?)"] * len(request.gene_ids))
+        + ")"
+    )
+
+    with duckdb.connect(DATABASE_PATH, read_only=True) as connection:
+        query_relation = connection.sql(
+            query=query,
+            params=[
+                value
+                for gene in gene_ids
+                for value in (gene.chromosome_id, gene.gene_id)
+            ],
+        ).project(  # we don't need id or genome_id here
+            "chromosome_id",
+            "gene_id",
+            "tool",
+            "evalue",
+            "score",
+            "seed_ortholog",
+            "description",
+        )
+
+        # Collect and reorder results based on original input order
+        annotations = [
+            GeneAnnotation(
+                **{k: v for k, v in zip(GeneAnnotation.model_fields.keys(), result)}
+            )
+            for result in query_relation.fetchall()
+        ]
+
+        # Sort by the original order of gene_ids
+        ordered_annotations = sorted(
+            annotations,
+            key=lambda annotation: gene_id_order[
+                (annotation.chromosome_id, annotation.gene_id)
+            ],
+        )
+
+        return AnnotationsResponse(results=ordered_annotations)
+
+
+@app.post("/api/expression")
+async def get_expression_duckdb(request: ExpressionRequest) -> ExpressionResponse:
+    if len(request.gene_ids) == 0:
+        return ExpressionResponse(genes=[], samples=[], values=[])
+
+    gene_ids = GeneInfo.split_gene_ids_from_request(request.gene_ids)
+
+    gene_id_order = {
+        (gene.chromosome_id, gene.gene_id): i for i, gene in enumerate(gene_ids)
+    }
+
+    with duckdb.connect(DATABASE_PATH, read_only=True) as connection:
+        experiment = connection.sql(
+            "SELECT relation_name FROM experiments WHERE id = ?",
+            params=[request.experiment_id],
+        ).fetchone()[0]
+
+        samples_ordered = [
+            SampleInfo(
+                experiment=experiment,
+                sample_id=sample[0],
+                reference=sample[1],
+                sequencing_id=sample[2],
+                condition=sample[3],
+            )
+            for sample in connection.sql(
+                "SELECT id, reference, sample_filename, condition from samples WHERE experiment_id = ?",
+                params=[request.experiment_id],
+            )
+            .order("id")
+            .fetchall()
+        ]
+
+        sample_info_order = {
+            sample.sample_id: i for i, sample in enumerate(samples_ordered)
+        }
+
+        query = (
+            f"SELECT sample_id, chromosome_id, gene_id, sum(tpm) as tpm from {experiment} "
+            f"WHERE (chromosome_id, gene_id) IN ({", ".join(["(?, ?)"] * len(gene_ids))}) GROUP BY sample_id, chromosome_id, gene_id"
+        )
+
+        query_relation = connection.sql(
+            query=query,
+            params=[
+                value
+                for gene in gene_ids
+                for value in (gene.chromosome_id, gene.gene_id)
+            ],
+        )
+
+        gene_infos = [
+            GeneInfo(chromosome_id=gene[0], gene_id=gene[1])
+            for gene in query_relation.project("chromosome_id", "gene_id")
+            .distinct()
+            .order("chromosome_id, gene_id")
+            .fetchall()
+        ]
+
+        results = query_relation.project(
+            "chromosome_id", "gene_id", "sample_id", "tpm"
+        ).fetchall()
+
+        results_ordered = sorted(
+            results,
+            # sort first by chromosome_id, gene_id, then by sample_id
+            key=lambda result: (
+                gene_id_order[(result[0], result[1])],
+                sample_info_order[result[2]],
+            ),
+        )
+
+        gene_infos_ordered = sorted(
+            gene_infos,
+            key=lambda gene_info: gene_id_order[
+                (gene_info.chromosome_id, gene_info.gene_id)
+            ],
+        )
+
+        return ExpressionResponse(
+            samples=samples_ordered,
+            genes=gene_infos_ordered,
+            values=[x[3] for x in results_ordered],
+        )
--- src/plantgenie_api/models.py
+++ src/plantgenie_api/models.py
@@ -1,21 +1,154 @@
-from typing import List, Optional
+from __future__ import annotations
 
-from pydantic import BaseModel, Field
+from typing import List, Optional, Tuple
 
-
-class PlantGenieBase(BaseModel):
-    pass
-
-
-class AnnotationsRequest(PlantGenieBase):
-    species: str
-    gene_ids: List[str] = Field(alias="geneIds")
+from pydantic import BaseModel, Field, ConfigDict
+from datetime import date
 
 
-class GeneAnnotation(PlantGenieBase):
+class PlantGenieModel(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+
+
+class GeneAnnotation(PlantGenieModel):
+    chromosome_id: str = Field(alias="chromosomeId")
     gene_id: str = Field(alias="geneId")
-    preferred_name: str = Field(alias="preferredName")
+    tool: Optional[str]
     evalue: Optional[float]
     score: Optional[float]
     seed_ortholog: Optional[str]
     description: Optional[str]
+
+
+class AnnotationsRequest(PlantGenieModel):
+    species: str
+    gene_ids: List[str] = Field(alias="geneIds")
+
+
+class AnnotationsResponse(PlantGenieModel):
+    results: List[GeneAnnotation]
+
+
+class GeneInfo(PlantGenieModel):
+    chromosome_id: str = Field(alias="chromosomeId")
+    gene_id: str = Field(alias="geneId")
+
+    @staticmethod
+    def split_from_request(gene_id_request: Tuple[str, str]) -> GeneInfo:
+        """
+        Gene IDs normally come in like this:
+            {chromosome_id}_{gene_id}
+
+        The gene_id part has no '_' inside of it, so it works to split on '_' and take
+        the last one for the gene_id and join the chromosome_id part on '_' and return it.
+        """
+        gene_id_split = gene_id_request.split("_")
+
+        if len(gene_id_split) < 2:
+            raise IndexError(
+                f"gene_id: {gene_id_request} split on '_' not at least length 2"
+            )
+
+        return GeneInfo(
+            chromosome_id="_".join(gene_id_split[:-1]), gene_id=gene_id_split[-1]
+        )
+
+    @staticmethod
+    def split_gene_ids_from_request(gene_ids: List[str]) -> List[GeneInfo]:
+        return [GeneInfo.split_from_request(gene_id) for gene_id in gene_ids]
+
+
+class GeneList(BaseModel):
+    species: str
+    experiment: str
+    gene_ids: List[str] = Field(alias="geneIds")
+
+    def gene_ids_to_gene_infos(self) -> List[GeneInfo]:
+        return [GeneInfo.split_from_request(gene_id) for gene_id in self.gene_ids]
+
+
+class SampleInfo(PlantGenieModel):
+    experiment: str
+    sample_id: int = Field(alias="sampleId")
+    reference: str
+    sequencing_id: str = Field(alias="sequencingId")
+    condition: str
+
+
+class ExpressionRequest(PlantGenieModel):
+    species: str
+    experiment_id: int = Field(alias="experimentId")
+    gene_ids: List[str] = Field(alias="geneIds", default=[])
+
+
+class ExpressionResponse(PlantGenieModel):
+    genes: List[GeneInfo]
+    samples: List[SampleInfo]
+    values: List[float]
+
+
+class GenesResponse(PlantGenieModel):
+    results: List[GeneAnnotation]
+
+
+class GenomeSequence(PlantGenieModel):
+    species: str
+    version: str
+    chromosome: str
+    begin: int
+    end: int
+    sequence: str
+
+
+class GenomeSequenceRequest(PlantGenieModel):
+    species: str = Field(description="species to query")
+    version: str = Field(description="genome version for the given species")
+    chromosome: str = Field(description="chromosome to extract")
+    begin: int = Field(description="starting index (inclusive)")
+    end: int = Field(description="ending index (inclusive)")
+
+
+class GenomeSequenceResponse(PlantGenieModel):
+    name: str
+    sequence: str
+
+
+# class AvailableGenome(PlantGenieModel):
+#     species: str
+#     version: str
+
+
+# class AvailableGenomesResponse(PlantGenieModel):
+#     genomes: List[AvailableGenome]
+
+
+class AvailableChromosomesRequest(PlantGenieModel):
+    species: str
+    version: str
+
+
+class AvailableChromosomesResponse(PlantGenieModel):
+    chromosomes: List[str]
+
+
+class AvailableSpecies(PlantGenieModel):
+    species_id: int = Field(alias="id")
+    species_name: str = Field(alias="speciesName")
+    species_abbreviation: str = Field(alias="speciesAbbreviation")
+    avatar_path: str = Field(alias="avatarPath")
+
+class AvailableSpeciesResponse(PlantGenieModel):
+    species: List[AvailableSpecies]
+
+class AvailableGenome(PlantGenieModel):
+    genome_id: int = Field(alias="id")
+    species_id: int = Field(alias="speciesId")
+    species_name: str = Field(alias="speciesName")
+    version: str
+    publication_date: Optional[date] = Field(alias="publicationDate")
+    doi: Optional[str]
+
+
+
+class AvailableGenomesResponse(PlantGenieModel):
+    genomes: List[AvailableGenome]
