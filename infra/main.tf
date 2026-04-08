71b591cb7f535d47005ac99b18e05596889e85bd
Jamie McCann <jamie.mccann@umu.se>
2026-04-08 12:02:58 +0200

    Added security groups and network data sources, updated workspace

--- infra/main.tf
+++ infra/main.tf
@@ -14,10 +14,75 @@ terraform {
 
   cloud {
     organization = "upsc-plantgenie"
+    # workspaces {
+    #   name = "north-dev"
+    # }
     workspaces {
-      name = "plantgenie-north"
+      tags = [ "plantgenie" ]
     }
   }
+}
+
+data "openstack_networking_network_v2" "external" {
+  name = var.external_network_name
+}
+
+data "openstack_networking_network_v2" "internal" {
+  name = var.internal_network_name
+}
+
+data "openstack_networking_subnet_v2" "internal" {
+  network_id = data.openstack_networking_network_v2.internal.id
+  ip_version = 4
+}
+
+resource "openstack_networking_secgroup_v2" "external_traffic" {
+  name        = "${terraform.workspace}-${var.application_name}-external"
+  description = "security group for web proxy instance"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "external_ssh_traffic" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol  = "tcp"
+
+  port_range_min = 22
+  port_range_max = 22
+
+  remote_ip_prefix = "130.239.0.0/16"
+
+  security_group_id = openstack_networking_secgroup_v2.external_traffic.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "external_http_traffic" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol  = "tcp"
+
+  port_range_min = 80
+  port_range_max = 80
+
+  remote_ip_prefix = "0.0.0.0/0"
+
+  security_group_id = openstack_networking_secgroup_v2.external_traffic.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "external_https_traffic" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol  = "tcp"
+
+  port_range_min = 443
+  port_range_max = 443
+
+  remote_ip_prefix = "0.0.0.0/0"
+
+  security_group_id = openstack_networking_secgroup_v2.external_traffic.id
+}
+
+resource "openstack_networking_secgroup_v2" "internal_traffic" {
+  name        = "${terraform.workspace}-${var.application_name}-internal"
+  description = "security group for ssh and private network access"
 }
 
 resource "tls_private_key" "ssh" {
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,13 @@ variable "application_name" {
   description = "Name of the application, used to prefix resource names"
   type        = string
 }
+
+variable "internal_network_name" {
+  description = "Name of the internal IPv4 network"
+  type        = string
+}
+
+variable "external_network_name" {
+  description = "Name of the external IPv4 network"
+  type        = string
+}
