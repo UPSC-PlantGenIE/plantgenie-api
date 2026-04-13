3dca55bfc08f18c8cc3bd4160ed46a984e211dd0
Jamie McCann <jamie.mccann@umu.se>
2026-04-13 07:49:49 +0200

    get_available_genomes now uses db dependency injection

--- src/plantgenie_api/api/v1/genome/routes.py
+++ src/plantgenie_api/api/v1/genome/routes.py
@@ -1,49 +1,32 @@
-from pathlib import Path
-
 from fastapi import APIRouter
 
-from plantgenie_api import ENV_DATA_PATH
 from plantgenie_api.api.v1.genome.models import (
     AvailableGenome,
     AvailableGenomesResponse,
 )
-
-from shared.services.database import SafeDuckDbConnection
-from shared.config import backend_config
-DATA_PATH = (
-    Path(ENV_DATA_PATH)
-    if ENV_DATA_PATH
-    else Path(__file__).parent.parent / "example_data"
-)
-
-DATABASE_PATH = DATA_PATH / "plantgenie-backend.db"
-
+from plantgenie_api.dependencies import DatabaseDep
 
 router = APIRouter(prefix="/genome")
 
 
 @router.get(path="/available-genomes", tags=["v1", "genome"])
-async def get_available_genomes() -> AvailableGenomesResponse:
-    with SafeDuckDbConnection(
-        f"{backend_config.get("DATA_PATH")}/{backend_config.get("DATABASE_NAME")}",
-        allowed_directories=[backend_config.get("DATA_PATH")],
-        read_only=True,
-    ) as connection:
-        query_relation = connection.sql(
-            "SELECT * FROM genomes JOIN species ON (genomes.species_id = species.id);"
-        ).project(
-            "id", "species_id", "species_name", "version", "publication_date", "doi"
-        )
-        print(query_relation)
+async def get_available_genomes(
+    db_connection: DatabaseDep,
+) -> AvailableGenomesResponse:
+    query_relation = db_connection.sql(
+        "SELECT * FROM genomes JOIN species ON (genomes.species_id = species.id);"
+    ).project(
+        "id", "species_id", "species_name", "version", "publication_date", "doi"
+    )
 
-        return AvailableGenomesResponse(
-            genomes=[
-                AvailableGenome(
-                    **{
-                        k: v
-                        for k, v in zip(AvailableGenome.model_fields.keys(), result)
-                    }
-                )
-                for result in query_relation.fetchall()
-            ]
-        )
+    return AvailableGenomesResponse(
+        genomes=[
+            AvailableGenome(
+                **{
+                    k: v
+                    for k, v in zip(AvailableGenome.model_fields.keys(), result)
+                }
+            )
+            for result in query_relation.fetchall()
+        ]
+    )
